a813c6ced932119eb4da27741323ed9ad42e54d4
niry1 <niry.hoareau@gmail.com>
2022-02-05 23:49:50 +0100

    stream kafka producer

--- stream/csv_to_kafka/csv_to_kafka.py
+++ stream/csv_to_kafka/csv_to_kafka.py
@@ -3,15 +3,14 @@ import glob
 import json
 
 from kafka import KafkaProducer
+from kafka import KafkaConsumer
 
-def row_to_kafka(data,producer):
-    data_dict = data.to_dict(orient="records")
-    for row in data_dict:
-        producer.send(b"metrics_changerv1",str(row))
+producer = KafkaProducer(bootstrap_servers = 'kafka:9092')
 
-producer = KafkaProducer(bootstrap_servers='kafka:9092',value_serializer =lambda x:json.dumps(x).encode("utf-8"))
-
-for file in glob.glob("/app/sample/*.csv"):
-    data = pd.read_csv(file, sep=";")
-    for row in data.to_dict("records"):
-        producer.send(b"metrics_changerv1",str(row))
+for file in glob.glob('/app/sample/*.csv'):
+    print("traitement du fichier : " + file)
+    data = pd.read_csv(file, sep=';')
+    for row in data.to_dict('records'):
+        producer.send('metrics_jems_kafka', json.dumps(str(row)).encode('utf-8'))
+        producer.flush()
+        print(row)
